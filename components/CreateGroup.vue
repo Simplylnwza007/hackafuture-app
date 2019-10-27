471b567782c96c8eb8fc0c19edbcc0eaaed51f35
Nitipat Suttirak <nitipat.suttirak@gmail.com>
2019-10-27 16:33:20 +0700

    aaaaaa

--- components/CreateGroup.vue
+++ components/CreateGroup.vue
@@ -1,11 +1,24 @@
 <template>
-  <div id="createGroup">
-    <!-- <div class="flex-layout">
+  <div id="createGroup" :v-if="!isSubmit">
+    <div class="flex-layout">
       <span>จาก</span>
-      <select name="">
-        <option value=""></option>
+      <select class="input" name="group_select" style="margin-left:110px">
+        <option value="เงินบริจาคน้ำท่วม">
+          เงินบริจาคน้ำท่วม
+        </option>
       </select>
-    </div> -->
+    </div>
+    <div class="flex-layout" style="margin-top:40px">
+      <span>ไปยัง</span>
+      <input class="input" style="margin-left:85px">
+    </div>
+    <div class="flex-layout" style="margin-top:40px">
+      <span>จำนวน</span>
+      <input class="input" type="number">
+    </div>
+    <div class="btn" @click="isSubmit = true">
+      <span>โอนเงิน</span>
+    </div>
   </div>
 </template>
 
@@ -14,24 +27,54 @@ export default {
   name: 'CreateGroup',
   components: {},
   props: {},
-  data() {
-    return {}
+  data () {
+    return {
+      isSubmit: false
+    }
   },
-  mounted() {},
+  mounted () {},
   methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
 #createGroup {
+  font-size: 52px;
   width: 1080px;
   height: 600px;
   background-color: white;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  padding: 20px;
+  padding: 60px 40px 40px 40px;
   .flex-layout {
     display: flex;
+  }
+  .input {
+    margin-left: 50px;
+    width: 800px;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding-left: 20px;
+    option {
+      width: 800px;
+      font-size: 52px;
+    }
+  }
+  .btn {
+    width: 600px;
+    border-radius: 50px;
+    height: 120px;
+    background-color: cornflowerblue;
+    margin-top: 40px;
+    margin-left: 220px;
+    margin-right: auto;
+    cursor: pointer;
+    position: relative;
+    padding: 20px 80px;
+    span {
+      font-size: 50px;
+      color: white;
+    }
   }
 }
 </style>
